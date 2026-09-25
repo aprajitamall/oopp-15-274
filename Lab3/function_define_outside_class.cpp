b01593d47530b0bf35aaaf19f864e11d0819b1ab
Aprajita Mall <aprajitamall2709@gmail.com>
2026-09-25 09:19:22 +0530

    function define outside class

---
+++ Lab3/function_define_outside_class.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class Time
+{
+private:
+    int hh, mm, ss;
+
+public:
+    void input(int h = 0, int m = 0, int s = 0);
+    void show();
+};
+
+void Time::input(int h, int m, int s)
+{
+    hh = h;
+    mm = m;
+    ss = s;
+}
+
+void Time::show()
+{
+    cout << hh << ":" << mm << ":" << ss << endl;
+}
+
+int main()
+{
+    Time t1, t2;
+
+    t1.input(10, 30, 45);
+
+    
+    t2.input();
+
+    cout << "Time 1: ";
+    t1.show();
+
+    cout << "Time 2: ";
+    t2.show();
+
+    return 0;
+}
